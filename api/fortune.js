6fb845f645c6804512b973c7caa56ebff46daac8
trendycandy <104425495+trendycandy@users.noreply.github.com>
2025-12-03 11:13:58 +0900

    Update fortune.js

--- api/fortune.js
+++ api/fortune.js
@@ -79,7 +79,7 @@ module.exports = async (req, res) => {
         const apiResponse = await new Promise((resolve, reject) => {
             const options = {
                 hostname: 'generativelanguage.googleapis.com',
-                path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+                path: `/v1/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
